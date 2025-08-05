7265ef6aa9bf79a5b621e4d3a0a833389bd73a82
abhinashyadav005 <080bct005@ioepc.edu.np>
2025-08-05 21:11:24 +0545

    omit

---
+++ spanning tree/dijkstra.py
@@ -0,0 +1,57 @@
+import math 
+from queue import PriorityQueue
+
+
+G={
+    's':{'t':10, 'y': 5},
+    't':{'y':2, 'x':1},
+    'x':{'z':6},
+    'y':{'x':9, 't':3,'z':2},
+    'z':{'x':6, 's':7}
+}
+
+
+
+def INITIALIZE_SINGLE_SOURCE(G,s):
+    cost = dict()
+    prev = dict()
+    for vertex in G.keys():
+        cost[vertex] = math.inf
+        prev[vertex] = " "
+    cost[s] = 0
+    return cost, prev
+
+def RELAX(G, u, v, cost, prev):
+    if cost[v] > cost[u] + G[u][v]:
+        cost[v] = cost[u] + G[u][v]
+        prev[v] = u
+    return cost, prev
+
+
+def DJ(G, s):
+    cost, prev = INITIALIZE_SINGLE_SOURCE(G, s)
+    PQ = PriorityQueue()
+    for vertex in G.keys():
+        PQ.put((cost[vertex], vertex))
+    visited = []
+    while(len(visited) != len(G.keys())):
+        _, currentVertex = PQ.get()
+        visited.append(currentVertex)
+        for chimeki in G[currentVertex].keys():
+            if chimeki not in visited:
+                cost, prev = RELAX(G, currentVertex, chimeki, cost, prev)
+                PQ.put((cost[chimeki], chimeki))
+    return cost, prev
+
+s = 's'
+cost, prev = DJ(G, s)
+
+def reconstruct_path(vertex, prev):
+    path = vertex
+    while prev[vertex] != ' ':
+        path = prev[vertex] + '->' + path
+        vertex = prev[vertex]
+    return path
+for vertex in G.keys():
+    print(f'shortest path from {s} to {vertex}:{reconstruct_path(vertex,prev)}')
+    print(f'Cost is {cost[vertex]}')
